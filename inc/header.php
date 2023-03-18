a7a185bbeb063fb5637a8e1fdb8eec3c0701b618
Khun Tun <jajahuge890@gmail.com>
2023-03-18 20:19:58 +0630

    last updated

---
+++ inc/header.php
@@ -0,0 +1,75 @@
+<?php
+$filepath = realpath(dirname(__FILE__));
+include_once $filepath."/../lib/Session.php";
+Session::init();
+
+
+
+spl_autoload_register(function($classes){
+  include ('classes/'. $classes . ".php");
+});
+
+$users = new Users();
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>User Management Application</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link href="assets/css/fontawesome-free-5.15.4-web/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+
+
+<?php
+if (isset($_GET['action']) && $_GET['action'] == 'logout') {
+    Session::destroy();
+}
+ ?>
+
+
+    <div class="container p-3">
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark  card-header">
+        <a class="navbar-brand ps-2" href="index.php"><i class="fas fa-home me-2"></i>Home</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#Mynav">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-end" id="Mynav">
+          <ul class="navbar-nav">
+            <?php if (Session::get('id') == TRUE) { ?>
+              <?php if (Session::get('roleid') == '1') { ?>
+                  <li class="nav-item">
+                      <a class="nav-link" href="index.php"><i class="fas fa-users me-2"></i>User Lists</span></a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="manageUser.php"><i class="fas fa-user me-2"></i>Manage</span></a>
+                  </li>  
+                  <li class="nav-item <?php $path = $_SERVER['SCRIPT_FILENAME']; $current = basename($path, '.php'); if ($current == 'addUser') { echo " active "; } ?>">
+                    <a class="nav-link" href="addUser.php"><i class="fas fa-user-plus me-2"></i>Add User</span></a>
+                  </li>
+            <?php  } ?>
+                <li class="nav-item <?php $path = $_SERVER['SCRIPT_FILENAME']; $current = basename($path, '.php'); if ($current == 'profile') { echo " active "; } ?>">
+                  <a class="nav-link" href="profile.php?id=<?php echo Session::get("id"); ?>"><i class="fab fa-500px me-2"></i>Profile <span class="sr-only">(current)</span></a>
+                </li>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="?action=logout"><i class="fas fa-sign-out-alt me-2"></i>Logout</a>
+                </li>
+            <?php }else{ ?>
+                <li class="nav-item <?php $path = $_SERVER['SCRIPT_FILENAME']; $current = basename($path, '.php'); if ($current == 'register') { echo " active "; } ?>">
+                  <a class="nav-link" href="register.php"><i class="fas fa-user-plus me-2"></i>Register</a>
+                </li>
+                <li class="nav-item <?php $path = $_SERVER['SCRIPT_FILENAME']; $current = basename($path, '.php'); if ($current == 'login') { echo " active "; } ?>">
+                  <a class="nav-link" href="login.php"><i class="fas fa-sign-in-alt me-2"></i>Login</a>
+                </li>
+            <?php } ?>
+          </ul>
+        </div>
+      </nav>
---
+++ index.php
@@ -0,0 +1,175 @@
+<?php
+include_once 'inc/header.php';
+include_once 'classes/Role.php';
+Session::CheckSession();
+
+
+$logMsg = Session::get('logMsg');
+if (isset($logMsg)) {
+  echo $logMsg;
+}
+$msg = Session::get('msg');
+if (isset($msg)) {
+  echo $msg;
+}
+Session::set("msg", NULL);
+Session::set("logMsg", NULL);
+?>
+<?php
+
+if (isset($_GET['remove'])) {
+  $remove = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['remove']);
+  $removeUser = $users->deleteUserById($remove);
+}
+
+if (isset($removeUser)) {
+  echo $removeUser;
+}
+if (isset($_GET['deactive'])) {
+  $deactive = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['deactive']);
+  $deactiveId = $users->userDeactiveByAdmin($deactive);
+}
+
+if (isset($deactiveId)) {
+  echo $deactiveId;
+}
+if (isset($_GET['active'])) {
+  $active = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['active']);
+  $activeId = $users->userActiveByAdmin($active);
+}
+
+if (isset($activeId)) {
+  echo $activeId;
+}
+
+ ?>
+      <div class="card ">
+        <div class="card-header">
+            <h3>
+              <i class="fas fa-users me-2"></i>User Lists 
+              <span class="float-end">Welcome! 
+                  <strong>
+                    <span class="badge  bg-secondary text-white"><?php $username = Session::get('username'); if (isset($username)) { echo $username; } ?></span>
+                  </strong>
+              </span>
+          </h3>
+        </div>
+        <div class="card-body px-2">
+          <table id="myTable" class="table table-striped table-bordered" style="width:100%">
+                  <thead>
+                    <tr> 
+                        <th  class="text-center">SL</th>
+                        <th  class="text-center">Name</th>
+                        <th  class="text-center">Username</th>
+                        <th  class="text-center">Email Address</th>
+                        <th  class="text-center">Mobile</th>
+                        <th  class="text-center">Status</th>
+                        <th  class="text-center">Created_At</th>
+                        <th  class="text-center" width='25%'>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php $allUser = $users->selectAllUserData(); if ($allUser) { $i = 0; foreach ($allUser as  $value) { $i++; ?>
+                      <tr class="text-center" <?php if (Session::get("id") == $value->id) { echo "style='background:#d9edf7' ";} ?>>
+                        <td><?php echo $i; ?></td>
+                        <td><?php echo $value->name; ?></td>
+                        <td><?php echo $value->username; ?> <br>
+
+
+                        <?php 
+                                  $obj = new Role();
+                                  $rows = $obj->fetchRole(); 
+
+                            ?>
+                             <?php foreach ($rows as $row): ?>
+                              
+                                  <?php if(Session::get("id") == $row->value  && Session::get("roleid") == '1'): ?>
+                                      <span class="badge  bg-info text-white">
+                                      <?= $row->role ?>
+                                  </span>
+
+                                  <?php elseif(Session::get("id") == $row->value  && Session::get("roleid") == '2'): ?>
+                                      <span class="badge  bg-success text-white">
+                                      <?= $row->role ?>
+                                  </span>
+
+                                  <?php elseif(Session::get("id") == $row->value  && Session::get("roleid") == '3'): ?>
+                                      <span class="badge  bg-primary text-white">
+                                      <?= $row->role ?>
+                                  </span>
+
+                                  <?php elseif(Session::get("id") == $row->value  && Session::get("roleid") == '4'): ?>
+                                      <span class="badge  bg-primary text-white">
+                                      <?= $row->role ?>
+                                  </span>
+
+                                  <?php elseif(Session::get("id") == $row->value  && Session::get("roleid") == '5'): ?>
+                                      <span class="badge  bg-primary text-white">
+                                      <?= $row->role ?>
+                                  </span>
+
+                                  <?php elseif(Session::get("id") == $row->value  && Session::get("roleid") == '6'): ?>
+                                      <span class="badge  bg-primary text-white">
+                                      <?= $row->role ?>
+                                  </span>
+
+                                  <?php else: ?>
+                                      <span class="badge  bg-primary text-white">
+                                      <?=  $row->role ?>
+                                  </span>
+                                  <?php endif ?>
+
+                            <?php endforeach ?> 
+                            
+                        </td> 
+                        <td><?php echo $value->email; ?></td>
+
+                        <td><span class="badge  bg-secondary  text-white"><?php echo $value->mobile; ?></span></td>
+                        <td>
+                          <?php if ($value->isActive == '0') { ?>
+                            <span class="badge  bg-info text-white">Active</span>
+                          <?php }else{ ?>
+                            <span class="badge  bg-danger text-white">Deactive</span>
+                          <?php } ?>
+                        </td>
+                        <td><span class="badge bg-secondary text-white"><?php echo $users->formatDate($value->created_at);  ?></span></td>
+                        <td>
+                            <?php if ( Session::get("roleid") == '1') {?>
+                                  <a class="btn btn-success btn-sm" href="profile.php?id=<?php echo $value->id;?>">View</a>
+                                  <a class="btn btn-info btn-sm " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
+                                  <a onclick="return confirm('Are you sure to delete ?')" class="btn btn-danger <?php if (Session::get("id") == $value->id) { echo "disabled"; } ?> btn-sm " href="?remove=<?php echo $value->id;?>">Remove</a>
+                                <?php if ($value->isActive == '0') {  ?>
+                                  <a onclick="return confirm('Are you sure to deactivate ?')" class="btn btn-warning <?php if (Session::get("id") == $value->id) { echo "disabled"; } ?> btn-sm " href="?deactive=<?php echo $value->id;?>">Disable</a>
+                                <?php } elseif($value->isActive == '1'){?>
+                                  <a onclick="return confirm('Are you sure to activate ?')" class="btn btn-secondary <?php if (Session::get("id") == $value->id) { echo "disabled"; } ?> btn-sm " href="?active=<?php echo $value->id;?>">Active</a>
+                                <?php } ?>
+
+                              <?php  }elseif(Session::get("id") == $value->id  && Session::get("roleid") == '2'){ ?>
+                                <a class="btn btn-success btn-sm " href="profile.php?id=<?php echo $value->id;?>">View</a>
+                                <a class="btn btn-info btn-sm " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
+                              <?php  }elseif( Session::get("roleid") == '2'){ ?>
+                                <a class="btn btn-success btn-sm <?php if ($value->roleid == '1') { echo "disabled"; } ?>" href="profile.php?id=<?php echo $value->id;?>">View</a>
+                                <a class="btn btn-info btn-sm <?php if ($value->roleid == '1') { echo "disabled"; } ?> " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
+                              <?php }elseif(Session::get("id") == $value->id  && Session::get("roleid") == '3'){ ?>
+                                <a class="btn btn-success btn-sm " href="profile.php?id=<?php echo $value->id;?>">View</a>
+                                <a class="btn btn-info btn-sm " href="profile.php?id=<?php echo $value->id;?>">Edit</a>
+                              <?php }else{ ?>
+                                <a class="btn btn-success btn-sm <?php if ($value->roleid == '1') { echo "disabled";  } ?> " href="profile.php?id=<?php echo $value->id;?>">View</a>
+                            <?php } ?>
+                        </td>
+                      </tr>
+                    <?php } }else{ ?>
+                      <tr class="text-center">
+                          <td>No user availabe now !</td>
+                      </tr>
+                    <?php } ?>
+                  </tbody>
+              </table>
+        </div>
+      </div>
+
+
+
+  <?php
+  include 'inc/footer.php';
+  ?>
